da552a5d2f7189bc19f409127c1685bffd14d821
Christian Weiss <c.weiss@c3-ls.com>
2018-09-10 19:38:40 +0200

    allow selecting framework when launching samples

--- launch-sample.ps1
+++ launch-sample.ps1
@@ -1,25 +1,33 @@
+[CmdletBinding(PositionalBinding = $false)]
+param(
+    [string] $Framework = "netcoreapp2.1"
+)
+
 dotnet build
 if ($LASTEXITCODE -ne 0) { throw "build error" }
 
+Write-Host "Launching samples with framework $Framework"
+
 Start-Process `
     -FilePath powershell.exe `
-    -ArgumentList @( "dotnet run --no-restore --no-build; Read-Host 'Press enter to exit'" ) `
+    -ArgumentList @( "dotnet run -f $Framework --no-build; Read-Host 'Press enter to exit'" ) `
     -WorkingDirectory "samples\CustomersApi"
 
 Start-Process `
     -FilePath powershell.exe `
-    -ArgumentList @( "dotnet run --no-restore --no-build; Read-Host 'Press enter to exit'" ) `
+    -ArgumentList @( "dotnet run -f $Framework --no-build; Read-Host 'Press enter to exit'" ) `
     -WorkingDirectory "samples\OrdersApi"
 
 Start-Sleep -Seconds 2
 
 Start-Process `
     -FilePath powershell.exe `
-    -ArgumentList @( "dotnet run --no-restore --no-build; Read-Host 'Press enter to exit'" ) `
+    -ArgumentList @( "dotnet run -f $Framework --no-build; Read-Host 'Press enter to exit'" ) `
     -WorkingDirectory "samples\FrontendWeb"
 
-Start-Process `
-    -FilePath powershell.exe `
-    -ArgumentList @( "dotnet run --no-restore --no-build; Read-Host 'Press enter to exit'" ) `
-    -WorkingDirectory "samples\TrafficGenerator"
-
+if ($Framework -ne "netcoreapp2.0") {
+    Start-Process `
+        -FilePath powershell.exe `
+        -ArgumentList @( "dotnet run -f $Framework --no-build; Read-Host 'Press enter to exit'" ) `
+        -WorkingDirectory "samples\TrafficGenerator"
+}
