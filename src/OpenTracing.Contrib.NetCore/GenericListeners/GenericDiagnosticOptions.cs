423d556def1148538e4d5fa9af36f6c4626186fd
Christian Weiss <c.weiss@c3-ls.com>
2020-12-12 09:08:03 +0100

    GenericDiagnosticOptions.IgnoreEvent() was removed by mistake; PrivateAssets() was missing on System.Data.SqlClient for .NET 5; fixes broken release-notes link in NuGet package

--- src/OpenTracing.Contrib.NetCore/GenericListeners/GenericDiagnosticOptions.cs
+++ src/OpenTracing.Contrib.NetCore/GenericListeners/GenericDiagnosticOptions.cs
@@ -7,5 +7,21 @@ namespace OpenTracing.Contrib.NetCore.Configuration
         public HashSet<string> IgnoredListenerNames { get; } = new HashSet<string>();
 
         public Dictionary<string, HashSet<string>> IgnoredEvents { get; } = new Dictionary<string, HashSet<string>>();
+
+        public void IgnoreEvent(string diagnosticListenerName, string eventName)
+        {
+            if (diagnosticListenerName == null || eventName == null)
+                return;
+
+            HashSet<string> ignoredListenerEvents;
+
+            if (!IgnoredEvents.TryGetValue(diagnosticListenerName, out ignoredListenerEvents))
+            {
+                ignoredListenerEvents = new HashSet<string>();
+                IgnoredEvents.Add(diagnosticListenerName, ignoredListenerEvents);
+            }
+
+            ignoredListenerEvents.Add(eventName);
+        }
     }
 }
